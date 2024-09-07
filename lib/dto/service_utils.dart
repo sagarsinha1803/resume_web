c5cf3007f7b5a31b591e30ec22516b308d8724de
sagarsinha1803 <sagarsinha1803@gmail.com>
2024-09-07 23:41:16 +0530

    [feature/githubaction] - Added Service Content

--- lib/dto/service_utils.dart
+++ lib/dto/service_utils.dart
@@ -13,19 +13,51 @@ class ServiceUtils {
 List<ServiceUtils> serviceUtilsList = [
   ServiceUtils(
     image: "assets/icons8-web-96.png",
-    title: "FRONTEND DEVELOPMENT",
-    description:
-        "I have experience building good looking web or github pages using Flutter.",
+    title: "FLUTTER UI SERVICES",
+    description: r"""
+        <ul>
+          <li>Responsive Web design</li>
+          <li>Custom UI design for Android, iOS & Windows.</li>
+          <li>Integrate New Widgets</li>
+          <li>Optimizing Widgets</li>
+          <li>Bug Fixing</li>
+          <li>UI Performance Optimization</li>
+          <li>Clean Architecture Design</li>
+        </ul>
+        """,
   ),
   ServiceUtils(
     image: "assets/icons8-programming-96.png",
-    title: "BACKEND DEVELOPMENT",
-    description:
-        "I have experience building RESTful APIs using Flask and Springboot.",
+    title: "BACKEND SERVICES",
+    description: r"""
+        <ul>
+          <li>API Development</li>
+          <li>API Enhancement</li>
+          <li>API Integration</li>
+          <li>Cloud Deployment</li>
+          <li>Code Migration</li>
+          <li>Bug Fixing</li>
+          <li>Performance Optimization</li>
+          <li>Resource Scaling</li>
+          <li>Database Migration</li>
+          <li>HLD and LLD Design</li>
+          <li>Clean Architecture Design</li>
+        </ul>
+        """,
   ),
   ServiceUtils(
     image: "assets/icons8-desktop-96.png",
-    title: "SCRIPTING",
-    description: "I have experience building scripts using Python.",
+    title: "SCRIPTING SERVICES",
+    description: r"""
+        <ul>
+          <li>Web Scraping</li>
+          <li>Data Extraction</li>
+          <li>Network Automation</li>
+          <li>Bug Fixing</li>
+          <li>Code Optimization</li>
+          <li>Automation Script Development</li>
+          <li>Script Migration</li>
+        </ul>
+        """,
   ),
 ];
--- lib/service_section/service_card.dart
+++ lib/service_section/service_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:resume_web/constants/colors.dart';
 import 'package:resume_web/dto/service_utils.dart';
@@ -16,7 +17,7 @@ class ServiceCard extends StatelessWidget {
       padding: const EdgeInsets.only(top: 5, bottom: 5),
       child: Container(
         decoration: BoxDecoration(
-          color: CustomColor.hintDark.withOpacity(0.3),
+          color: const Color(0xFF1D1B20),
           borderRadius: BorderRadius.circular(30),
         ),
         child: Column(
@@ -39,8 +40,9 @@ class ServiceCard extends StatelessWidget {
                         height: 150,
                         width: 150,
                         decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: CustomColor.hintDark),
+                          shape: BoxShape.circle,
+                          color: Color(0xFF1D1B20),
+                        ),
                         child: Image.asset(
                           service.image,
                           height: 150,
@@ -51,9 +53,9 @@ class ServiceCard extends StatelessWidget {
                     Text(
                       service.title,
                       style: GoogleFonts.sriracha(
-                        color: CustomColor.bgLight1,
-                        fontSize: 22,
-                        wordSpacing: 2,
+                        color: const Color(0xFF1D1B20),
+                        fontSize: 20,
+                        wordSpacing: 5,
                       ),
                     ),
                   ],
@@ -61,7 +63,20 @@ class ServiceCard extends StatelessWidget {
               ),
             ),
 
-            Text(service.description),
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Html(
+                data: service.description,
+                // Optionally, style your HTML content
+                style: {
+                  "li": Style(
+                    fontSize: FontSize(15.0),
+                    fontWeight: FontWeight.w700,
+                  ),
+                  // Add more tag styles as needed
+                },
+              ),
+            ),
           ],
         ),
       ),
